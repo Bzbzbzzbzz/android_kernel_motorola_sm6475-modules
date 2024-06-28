b017e5f080074687199acc73e50811a291cb51ed
xuwt2 <xuwt2@lenovo.com>
2024-06-28 13:50:29 +0800

    DLKM: New Moto Charger Driver for Qcom glink Arch
    mmi_glink_charger:
    1, Moto Common charger driver is used to implement moto charge feature.
    2, Charger device glink driver is used to implement charge IC Date access.
    3, Battery host driver is designed to access battery information
    and customization of Moto sepcial functins for battery.
    4, Device Glink class:
    A class of charger devices on the AP driver. Addn any charging devices.
    such as
    main_batt/flip_batt/charger pump/balance charge/buck charger/wireless charge
    qti_glink_charger_v2:
    A common Glink module is used for charger data interaction
    between the AP core and ADSP core.
    ------------------------------------------------------------------------
    Kernel           [mmk_glink_charger]
                              |
     [HeartBeat_Work]  [notifier_chain]  [Moto_ops]
                              |
                       [Device_Class]
                              |
    [batt_dev] [balance_dev] [charge_pump_dev] [buck_dev] [wireless_dev] ....
                              |
                    [qti_glink_charger_v2]
    -------------------------------------------------------------------------
    ADSP            [Battmngr_charge_module]
    
    Change-Id: Ibab04caa186593c34740979d9fc9c5a0da3a1994
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3013825
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/power/mmi_glink_charger/device_glink/charge_pump_glink.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2024 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __CHARGE_PUMP_H__
+#define __CHARGE_PUMP_H__
+
+#endif
